eb986442476dd79510971bce1dd20c74c9192d6f
Jazzberries <anime.flick.chick@gmail.com>
2022-03-27 16:48:38 +1100

    Puzzled out save to local storage and showing data from previous history on click

--- assets/script/script.js
+++ assets/script/script.js
@@ -4,17 +4,13 @@ console.log("This script has loaded");
 
 const APIKey = "6b826d4c34586e17f4f669d088a91aed";
 
+// setting global variables
+
 let searchFormEl = document.getElementById("search-form");
 
-let savedData = {};
+// let cityInputVal = document.getElementById("city-search-input");
 
-let weatherForecastData = {};
-
-// var cityInput = "sydney"
-
-// let latLonUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityInput + "&appid=" + APIKey;
-
-let cityInputVal = document.getElementById("city-search-input");
+let cityInputVal = [];
 
 let cityName = document.getElementById("current-city-name");
 
@@ -27,14 +23,20 @@ let searchBtn = document.getElementById("search-btn");
 
 let previousSearches = document.getElementById("previous-searches");
 
-let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
+let i = 0;
 
+// empty objects to store JSON data received from API
+
+let savedData = {};
+
+let weatherForecastData = {};
 
 
 //prevent form from refreshing page and prevent user from not entering a city
 
 function formSubmitHandler(event) {
     event.preventDefault();
+    event.stopPropagation();
 
     cityInputVal = document.getElementById("city-search-input").value;
 
@@ -45,17 +47,53 @@ function formSubmitHandler(event) {
 
     console.log(cityInputVal)
 
-    // searchHistory();
-
+    readThingy();
 
     getApi();
 
+}
+
+// function storeThingy() {
+
+//     localStorage.setItem("Thingy", cityInputVal);
+
+
+// }
+
+
+function readThingy() {
+
+    let retrieveThingy = [];
+
+    let savedCity = cityInputVal;
+
+    retrieveThingy.push(savedCity);
+
+    localStorage.setItem(savedCity, savedCity);
+
+
+
+    retrieveThingy = localStorage.getItem(savedCity, savedCity);
+
+    let printThingy = document.createElement("button");
+    printThingy.setAttribute("class", "searchHistoryBtn")
+    printThingy.textContent = cityInputVal;
+    printThingy.value = cityInputVal;
+    previousSearches.appendChild(printThingy);
+
+
+    // let searchInput = cityInputVal.value;
+    //     getApi(searchInput);
+    //     searchHistory.push(searchInput);
+    //     localStorage.setItem("search", JSON.stringify(searchHistory));
 }
 
 
 //get the API
 
 function getApi() {
+
+
 
     //API request based on city name to obtain the latitude and longitude
 
@@ -71,13 +109,14 @@ function getApi() {
             return response.json();
         })
 
-        //save the data from the latitude and longitude API request
+        //save the data from the latitude and longitude API request to empty object
+
         .then(function (data) {
             console.log(data)
 
             savedData = data;
 
-            //API request based on latitude and longitude from our first API request
+            //Second API request based on latitude and longitude that we obtained from our first API request
 
             return fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + savedData[0].lat + "&lon=" + savedData[0].lon + "&units=metric&exclude=hourly,minutely&appid=" + APIKey)
 
@@ -88,6 +127,8 @@ function getApi() {
             }
             return response.json();
         })
+
+        //save the second API request's data to the second empty object
         .then(function (data) {
 
             weatherForecastData = data
@@ -95,7 +136,6 @@ function getApi() {
             if (!Object.keys(weatherForecastData).length) {
                 console.log("No weather")
             } else {
-                // todayForecast = ""
 
                 printWeather();
 
@@ -108,6 +148,8 @@ function printWeather() {
 
     //print the current weather conditions as per the JSON response from the APIs
 
+    todayForecast.innerHTML = "";
+
     let todayForecastContainer = document.createElement("div");
     todayForecastContainer.classList.add("card", "bg-light", "text-dark", "p-2");
     todayForecastContainer.setAttribute("id", "current-container");
@@ -116,13 +158,11 @@ function printWeather() {
     let todayForecastCard = document.createElement('div');
     todayForecastCard.classList.add("card-body");
 
-    //current time  here
+    //get the current date from the UNIX code from the saved weatherForecastData stored in the weatherForecastData object
 
     let currentDate = new Date(weatherForecastData.current.dt * 1000);
 
     let day = currentDate.getDate();
-
-    // let month = currentDate.getMonth() + 1;
 
     let currentMonthOptions = { month: "long" };
 
@@ -131,12 +171,11 @@ function printWeather() {
 
     let year = currentDate.getFullYear();
 
+    //dispay the city name from the search and the current date
 
     cityName.innerHTML = savedData[0].name + " on " + day + " " + currentMonthFull + " " + year
 
-    // let currentCity = document.createElement("h2");
-    // currentCity.textContent = savedData[0].name + " ";
-    // cityName.appendChild(currentCity);
+    //create elements to display the desired weather details (weather icon, conditions, temp, wind speed, humidity and UV index)
 
     let currentForecast = document.getElementById("current-container")
 
@@ -148,7 +187,9 @@ function printWeather() {
     weatherIcon.setAttribute("alt", "Icon showing current weather status of " + weatherForecastData.current.weather[0].description);
     currentForecast.appendChild(weatherIcon);
 
-    let currentForecastDetails = ["Conditions: " + weatherForecastData.current.weather[0].main, "Temp: " + weatherForecastData.current.temp, "Wind: " + weatherForecastData.current.wind_speed, "Humidity: " + weatherForecastData.current.humidity, "Uv Index: " + weatherForecastData.current.uvi];
+    let currentForecastDetails = ["Conditions: " + weatherForecastData.current.weather[0].main, "Temp: " + weatherForecastData.current.temp + " ℃", "Wind: " + weatherForecastData.current.wind_speed + " kmph", "Humidity: " + weatherForecastData.current.humidity + " %", "UV Index: " + weatherForecastData.current.uvi];
+
+    //create an li element for each weather attribute conditions, temp, wind speed, humidity and UV index
 
     currentForecastDetails.forEach(function (current) {
         let liEl = document.createElement("li");
@@ -179,9 +220,7 @@ function printWeather() {
 
         liEls[4].setAttribute("class", "bg-danger", "bg-opacity-50", "text-white");
 
-    }
-
-    else {
+    } else {
 
         liEls[4].setAttribute("class", "bg-danger", "text-white");
 
@@ -219,6 +258,8 @@ function printWeather() {
 
     //future forecast. Loop over the weatherForecastData.daily object and return conditions, temp, wind, humidity and UV for the next five days
 
+    //loop through the daily weatherForecastData to get the five-day forecast. Skip over index 0 as that contains current date's daily details
+
     for (let i = 1; i < Object.keys(weatherForecastData.daily)[6]; i++) {
 
         //create bootstrap card for the five-day future forecast
@@ -228,28 +269,15 @@ function printWeather() {
         fiveDayForecastContainer.setAttribute("id", "five-day-container");
         futureForecast.append(fiveDayForecastContainer);
 
-        //create indivudal card elements with five-day forecast details on them
-
-
-        // fiveDayForecastCard.textContent = weatherForecastData.daily[i].humidity;
-        // fiveDayForecastContainer.appendChild(fiveDayForecastCard);
-
-        // let fiveDayFutureForecast = document.getElementById("five-day-container")
-
-        //future dates
+        //display future dates
 
         let futureDate = new Date(weatherForecastData.daily[i].dt * 1000);
-
 
         // console.log(futureDate);
 
         let futureDay = futureDate.getDate();
 
         // console.log(futureday);
-
-        // let futureMonth = futureDate.getMonth() + 1;
-
-        // console.log(futuremonth);
 
         let futureMonthOptions = { month: "long" };
 
@@ -261,8 +289,7 @@ function printWeather() {
 
         // console.log(futureyear);
 
-
-        // cityName.innerHTML = savedData[0].name + " on " + day + "/" + month + "/" + year
+        // create weather icons for future weather forecast
 
         let futureWeatherIcon = document.createElement("img");
         futureWeatherIcon.classList.add("future-weather-icon");
@@ -270,15 +297,14 @@ function printWeather() {
         futureWeatherIcon.setAttribute("alt", "Icon showing current weather status of " + weatherForecastData.daily[i].weather[0].description);
         fiveDayForecastContainer.appendChild(futureWeatherIcon);
 
+
+        //create indivudal card elements with five-day forecast details on them
+
         let futureForecastFragment = document.createDocumentFragment();
 
-        let futureForecastDetails = [futureDay + " " + futureMonthFull + " " + futureYear, "Conditions: " + weatherForecastData.daily[i].weather[0].main, "Temp: " + weatherForecastData.current.temp, "Wind: " + weatherForecastData.daily[i].wind_speed, "Humidity: " + weatherForecastData.daily[i].humidity];
+        let futureForecastDetails = [futureDay + " " + futureMonthFull + " " + futureYear, "Conditions: " + weatherForecastData.daily[i].weather[0].main, "Temp: " + weatherForecastData.current.temp + " ℃", "Wind: " + weatherForecastData.daily[i].wind_speed + " kmph", "Humidity: " + weatherForecastData.daily[i].humidity + " %"];
 
         futureForecastDetails.forEach(function (future) {
-
-            // let fiveDayFutureForecastCard = document.createElement('div');
-            // fiveDayFutureForecastCard.classList.add("card-body");
-            // fiveDayFutureForecast.appendChild(fiveDayFutureForecastCard);
 
             let liEl = document.createElement("li");
             liEl.textContent = future;
@@ -365,5 +391,54 @@ function printWeather() {
 //     currentCity.appendChild(currentDate);
 // }
 
+// function getHistory() {
+
+
+// let historyItem = document.createElement("p");
+
+// let searchItems = searchHistoryArray.push(cityInputVal);
+
+
+
+
+// historyItem.textContent = searchItems;
+// previousSearches.appendChild(historyItem);
+
+
+
+
+// let retrieveHistory = JSON.parse(localStorage.getItem(searchHistory)) || [];
+
+// searchHistory.push(retrieveHistory)
+
+
+
+// }
+
+// function init() {
+
+//     readThingy;
+// }
+
+
+previousSearches.addEventListener("click", function (event) {
+    if (event.target.className === "searchHistoryBtn") {
+        console.log("click!");
+
+        let savedHistory = Array.from(document.querySelectorAll(".searchHistoryBtn"));
+
+        cityInputVal = savedHistory[i].value;
+        // cityInputVal = "";
+        // localStorage.getItem("Thingy");
+        getApi();
+    }
+
+});
+// searchHistoryBtn.addEventListener("click", function (event) {
+//     event.stopPropagation();
+//     event.target(getApi);
+
+// });
+//submit button event listener to start everything off
 
 searchFormEl.addEventListener("submit", formSubmitHandler);
